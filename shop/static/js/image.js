a4869d58ebeb794586ff717dfbf89f8fd2d5f7b9
hoanganh121199 <anhvmh.vnist@gmail.com>
2021-08-17 16:38:40 +0700

    thêm trang chi tiết sản phẩm

---
+++ shop/static/js/image.js
@@ -0,0 +1,32 @@
+$(document).ready(function () {
+    console.log("!23")
+    const imgs = $('.wrap-slick3-dots li a');
+    const imgBtns = [...imgs];
+    let imgId = 1;
+    const imgshow = $('.preview-pic div');
+    console.log(imgBtns,imgshow[0].id);
+
+    imgshow[0].classList.add('active');
+    imgBtns.forEach((imgItem) => {
+        imgItem.addEventListener('click', (event) => {
+            event.preventDefault();
+            imgId = imgItem.dataset.target;
+            $('.preview-pic div').attr('class', 'tab-pane')
+            $(`#${imgId}`).attr('class', 'tab-pane active');
+        });
+    });
+    $('.carousel-inner div')[0].classList.add('active');
+    $('#itemslider').carousel({ interval: 10000 });
+
+    $('.carousel-showmanymoveone .item').each(function(){
+    var itemToClone = $(this);
+
+        for (var i=1;i<4;i++) {
+            itemToClone = itemToClone.next();
+            if (!itemToClone.length) {
+                itemToClone = $(this).siblings(':first');
+            }
+            itemToClone.children(':first-child').clone().addClass("cloneditem-"+(i)).appendTo($(this));
+        }
+    });
+});
